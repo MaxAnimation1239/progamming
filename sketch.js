0dd6975446c376143a1bf5eb5971a42bfe085746
MaxAnimation1239 <118488618+MaxAnimation1239@users.noreply.github.com>
2022-12-12 13:40:12 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  // put setup code here
+   var width = 700;
+   var height = 750;
+     createCanvas (width, height);
+     background('#E61C13');
+}
+
+function draw() {
+  // put drawing code here
+  if(mouseIsPressed === true){
+  fill('#F207CA');
+  stroke('#055299');
+  ellipse (mouseX, mouseY, );
+  }
+  else{
+    fill('#D8E629');
+    stroke('#298CE6');
+    square(mouseX,mouseY);
+  }
+}
